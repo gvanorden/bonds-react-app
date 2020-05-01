5a0379449e8af794ccc37206266bf2d6c576e74c
Gregg Van Orden <greggvanorden@Greggs-MacBook-Pro.local>
2020-05-01 12:21:12 -0400

    v1.4

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,79 +1,130 @@
 import React, { Component } from 'react';
-import { Jumbotron, ListGroup, Card, Table } from 'react-bootstrap';
+import { isMobile } from "react-device-detect";
+import { Jumbotron, ListGroup, Card } from 'react-bootstrap';
 import TDirectLogo from '../images/treasury-direct.png';
-import '../index.css'
+import '../styles/index.css'
 
 class HomePage extends Component {
-    state = {}
+    constructor(props) {
+        super(props);
+        this.state = {
+            windowWidth: null,
+            windowHeight: null
+        };
+
+        this.handleResize = this.handleResize.bind(this)
+        this.setClass = this.setClass.bind(this)
+        this.setBkgdSize = this.setBkgdSize.bind(this)
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.handleResize);
+
+        this.setState({ windowHeight: window.outerHeight, windowWidth: window.outerWidth })
+    }
+
+    handleResize() {
+        setTimeout(() => {
+            this.setState({ windowWidth: window.outerWidth, windowHeight: window.outerHeight })
+            console.log(this.state.windowWidth, this.state.windowHeight)
+        }, 100);
+    }
+
+    setClass(name) {
+        const { windowHeight, windowWidth } = this.state
+
+        if (isMobile) {
+            if (windowWidth < 450) {
+                return name + '-mobile-sm'
+            }
+            else if (isMobile && ((windowHeight > windowWidth) || (windowWidth <= 1024))) {
+                return name + '-mobile'
+            }
+            else {
+                return name + '-tablet'
+            }
+        }
+        else {
+            return name
+        }
+    }
+
+    setBkgdSize() {
+        const { windowWidth } = this.state
+
+        if (isMobile) {
+            if (windowWidth < 500) {
+                return { backgroundSize: '375%' }
+            }
+            else if (windowWidth <= 1024) {
+                return { backgroundSize: '175%' }
+            }
+            else if (windowWidth < 1224) {
+                return { backgroundSize: '150%' }
+            }
+            else {
+                return { backgroundSize: '125%' }
+            }
+        }
+        else {
+            return { backgroundSize: '125%' }
+        }
+    }
+
     render() {
+        const { setClass } = this
+
         return (
             <React.Fragment>
-                <Jumbotron className='roboto' style={{ color: '#343a40' }}>
-                    <h1 style={{ fontSize: '2em' }}>Quickly and easily price your savings bonds!</h1>
-                    <h1 style={{ fontSize: '1em' }}>VALUE, AGGREGATE, and EXPORT any set of I, E, EE Series Bonds or Savings Notes.</h1>
+                <Jumbotron className={setClass('home-jumbotron')} style={this.setBkgdSize()}>
+                    <h1>Quickly and easily price your savings bonds!</h1>
+                    <h2 style={{ fontSize: '1rem' }}>Value, Aggregate, and Export any set of I, E, EE Series Bonds or Savings Notes.</h2>
                     <div style={{ marginTop: '3rem' }}>
-                        <p style={{ marginBottom: '.5rem', marginLeft: '3em' }}>Choose an option:</p>
-                        <ListGroup style={{ width: '25%', marginLeft: '3em' }}>
-                            <ListGroup.Item><a style={{ color: '#ff420e', fontWeight: '500' }} href="/bond_pricing_individual">Price bonds individually</a></ListGroup.Item>
-                            <ListGroup.Item><a style={{ color: '#ff420e', fontWeight: '500' }} href="/bond_pricing_spreadsheet">Price bonds from spreadsheet</a></ListGroup.Item>
+                        <p style={{ float: 'left' }}>Choose an option:</p>
+                        <ListGroup className={setClass('home-choose-container')} >
+                            <ListGroup.Item className={setClass('home-choose')}><a style={{ color: '#ff420e', fontWeight: '500' }} href="/bond_pricing_individual">Price bonds individually</a></ListGroup.Item>
+                            <ListGroup.Item className={setClass('home-choose')}><a style={{ color: '#ff420e', fontWeight: '500' }} href="/bond_pricing_spreadsheet">Price bonds from spreadsheet</a></ListGroup.Item>
                         </ListGroup>
                     </div>
-                </Jumbotron>
-                <Card style={{ float: 'left', width: '47%', marginLeft: '2%' }}>
-                    <Card.Header style={{ background: 'rgba(0,0,0,.02)', fontSize: '1.25em', fontWeight: 'normal' }}>HOW WE DO IT</Card.Header>
-                    <Card.Body style={{ textAlign: 'justify', paddingBottom: '.8em' }}>
-                        <p>The <a target="_blank" rel="noopener noreferrer" href="https://www.treasurydirect.gov"><img style={{ width: '7.5em', marginRight: '.5em' }} src={TDirectLogo} alt="Treasury Direct" /></a>
-                        provides historical <a href="https://www.treasurydirect.gov/indiv/tools/tools_savingsbondvalues_historical.htm">value files</a> that cover every issued I, E/EE Series bonds and/or Savings Notes.
+                </Jumbotron >
+                <div className={setClass('home-container')} style={{ maxWidth: this.state.windowWidth }}>
+                    <Card className={setClass('home-card')}>
+                        <Card.Header className={'home-card-header'}>HOW WE DO IT</Card.Header>
+                        <Card.Body className={'home-card-body'}>
+                            <p><a target="_blank" rel="noopener noreferrer" href="https://www.treasurydirect.gov"><img style={{ width: '7.5rem', marginRight: '.5rem' }} src={TDirectLogo} alt="Treasury Direct" /></a>
+                        provides historical <a href="https://www.treasurydirect.gov/indiv/tools/tools_savingsbondvalues_historical.htm">value files</a> which contain pricing data for all issued I, E/EE Series bonds and/or Savings Notes.
                         We use a database built from these source files to evaluate the price of your bond(s).
                         </p>
-                        <ul>
-                            <li>100% accurate results</li>
-                            <li>View redemption value data between May 1992 and May 2020</li>
-                        </ul>
-                    </Card.Body>
-                </Card>
-                <Card style={{ float: 'left', width: '47%', marginLeft: '2%' }}>
-                    <Card.Header style={{ background: 'rgba(0,0,0,.02)', fontSize: '1.25em', fontWeight: 'normal' }}>WHAT YOU'LL NEED</Card.Header>
-                    <Card.Body style={{ textAlign: 'justify' }}>
-                        <div style={{ float: 'left', width: '100%' }}>
-                            <div style={{ float: 'left', width: '65%' }}>
-                                <p style={{ fontWeight: '500', marginBottom: '.25em' }}>MANDATORY</p>
-                                <ListGroup style={{ marginLeft: '1em' }} horizontal>
-                                    <ListGroup.Item className="wyn-list-item">
-                                        <p className="wyn-seperator">&#8250;</p><p className="wyn-text">&nbsp;Series</p>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item className="wyn-list-item" style={{ marginLeft: '1em' }}>
-                                        <p className="wyn-seperator">&#8250;</p><p className="wyn-text">&nbsp;Denomination</p>
-                                    </ListGroup.Item>
-                                    <ListGroup.Item className="wyn-list-item" style={{ marginLeft: '1em' }}>
-                                        <p className="wyn-seperator">&#8250;</p><p className="wyn-text">&nbsp;Issue Date</p>
-                                    </ListGroup.Item>
-                                </ListGroup>
-                            </div>
-                            <div style={{ float: 'left', width: '30%', marginLeft: '5%' }}>
-                                <p style={{ fontWeight: '500', marginBottom: '.25em' }}>OPTIONAL</p>
-                                <ListGroup style={{ marginLeft: '1em' }} >
-                                    <ListGroup.Item className="wyn-list-item">
-                                        <p className="wyn-seperator">&#8250;</p><p className="wyn-text">&nbsp;Serial Number</p>
-                                    </ListGroup.Item>
-                                </ListGroup>
-                            </div>
-                        </div>
-                        <div style={{ float: 'left', width: '100%', marginTop: '1em' }}>
-                            <p style={{ fontWeight: '500', marginBottom: '.25em' }}>FOR ASSISTANCE</p>
-                            <Table style={{ marginBottom: '0', width: 'auto', marginLeft: '1em' }}>
-                                <tr>
-                                    <td style={{ border: 'none', padding: '0' }}>Building a spreadsheet for import:</td>
-                                    <td style={{ border: 'none', padding: '0', paddingLeft: '1em' }}><a href="./instructions">Instructions</a></td>
-                                </tr>
-                                <tr>
-                                    <td style={{ border: 'none', padding: '0' }}>Locating these bond components:</td>
-                                    <td style={{ border: 'none', padding: '0', paddingLeft: '1em' }}><a href="./instructions">Bond Anatomy</a></td>
-                                </tr>
-                            </Table>
-                        </div>
-                    </Card.Body>
-                </Card>
+                            <ul>
+                                <li>100% accurate results</li>
+                                <li>View pricing data between May 1992 and May 2020</li>
+                            </ul>
+                        </Card.Body>
+                    </Card>
+                    <Card className={setClass('home-card')}>
+                        <Card.Header className={'home-card-header'}>WHAT YOU'LL NEED</Card.Header>
+                        <Card.Body className={'home-card-body'}>
+                            <p style={{ marginBottom: '.5rem' }}>On your bond, you'll need to locate the following items:</p>
+                            <p style={{ float: 'left', width: '50%', margin: '.5rem 0', fontWeight: '500' }}>MANDATORY</p>
+                            <p style={{ float: 'left', width: '50%', margin: '.5rem 0', fontWeight: '500' }}>OPTIONAL</p>
+                            <ListGroup style={{ float: 'left', width: '50%' }}>
+                                <ListGroup.Item className={'home-list-item'}>&#8250; Denomination</ListGroup.Item>
+                                <ListGroup.Item className={'home-list-item'}>&#8250; Issue Date</ListGroup.Item>
+                                <ListGroup.Item className={'home-list-item'}>&#8250; Series</ListGroup.Item>
+                            </ListGroup>
+                            <ListGroup style={{ float: 'left', width: '50%' }}>
+                                <ListGroup.Item className={'home-list-item'} >&#8250; Serial Number</ListGroup.Item>
+                            </ListGroup>
+                        </Card.Body>
+                    </Card>
+                    <Card className={setClass('home-card')}>
+                        <Card.Header className={'home-card-header'}>FOR ASSISTANCE</Card.Header>
+                        <Card.Body className={'home-card-body'}>
+                            <p style={{ marginBottom: '.5rem', textAlign: 'left' }}><a href="./instructions">Build a spreadsheet to import</a></p>
+                            <p style={{ marginBottom: '.5rem', textAlign: 'left' }}><a href="./instructions">Locate mandatory components on your bond</a></p>
+                        </Card.Body>
+                    </Card>
+                </div>
             </React.Fragment >
         )
     }
