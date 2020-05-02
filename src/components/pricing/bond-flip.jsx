d0d9f555e7747c543b2c414048190a6311f287d0
Gregg Van Orden <greggvanorden@Greggs-MacBook-Pro.local>
2020-05-02 18:10:15 -0400

    v1.20

--- src/components/pricing/bond-flip.jsx
+++ src/components/pricing/bond-flip.jsx
@@ -5,8 +5,10 @@ class BondFlip extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            windowWidth: null,
-            windowHeight: null
+            outerWidth: null,
+            outerHeight: null,
+            innerWidth: null,
+            innerHeight: null
         }
 
         this.handleResize = this.handleResize.bind(this)
@@ -15,20 +17,22 @@ class BondFlip extends Component {
     componentDidMount() {
         window.addEventListener("resize", this.handleResize);
 
-        this.setState({ windowWidth: window.outerWidth, windowHeight: window.outerHeight })
+        this.setState({ outerWidth: window.outerWidth, outerHeight: window.outerHeight, innerWidth: window.innerWidth, innerHeight: window.innerHeight })
     }
 
     handleResize() {
         setTimeout(() => {
-            this.setState({ windowWidth: window.outerWidth, windowHeight: window.outerHeight })
+            this.setState({ outerWidth: window.outerWidth, outerHeight: window.outerHeight, innerWidth: window.innerWidth, innerHeight: window.innerHeight })
         }, 250);
     }
 
     render() {
         return (
             <div style={{ marginTop: '3rem', width: '100%', textAlign: 'center' }}>
-                <div>{this.state.windowWidth}</div>
-                <div>{this.state.windowHeight}</div>
+                <div>{this.state.outerWidth}</div>
+                <div>{this.state.innerWidth}</div>
+                <div>{this.state.outerHeight}</div>
+                <div>{this.state.innerHeight}</div>
                 <p style={{ fontWeight: 'bold' }}>This app feature requires a wide screen view.</p>
                 <img className="flip-arrows" style={{ marginTop: '1rem' }} src={arrows} alt='Flip arrows for app view'></img>
             </div>
